a18c89f5b1b1a8b6aedd3ca5fb6290d334177bc1
Santhiago Santos <santhiagosantos7@gmail.com>
2024-07-07 11:22:21 -0300

    Adiciona arquivo com exercício 08 SQL

---
+++ Sprint 2/Exercicios/Exercicio 08.sql
@@ -0,0 +1,18 @@
+SELECT
+	tbvendedor.cdvdd,
+	tbvendedor.nmvdd
+FROM
+	tbvendas
+JOIN 
+	tbvendedor 
+ON 
+	tbvendas.cdvdd = tbvendedor.cdvdd
+WHERE
+	tbvendas.status = 'Concluído'
+GROUP BY
+	tbvendedor.cdvdd, tbvendedor.nmvdd
+ORDER BY
+	COUNT(tbvendas.cdvdd) DESC
+LIMIT
+	1
+
