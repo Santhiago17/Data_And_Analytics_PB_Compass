0c77a729eecc609d0bf8640b86d1b4ffba6620a3
Santhiago Santos <santhiagosantos7@gmail.com>
2025-04-02 15:18:17 -0300

    Remove token API

--- Sprint 7/Desafio/script_autenticacao_tmdb.py
+++ Sprint 7/Desafio/script_autenticacao_tmdb.py
@@ -5,7 +5,7 @@ url = "https://api.themoviedb.org/3/account"
 
 headers = {
     "accept": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OWYwNjI3YWNlMjI1NGE0NTM0MTcxN2U3Nzg0MTc1YSIsIm5iZiI6MTcyNjUwNzY4OS40MTAxOSwic3ViIjoiNjZlODY4N2JkN2JjY2E1MjRkYjEyZTY5Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.Nc3M1C__qCzMZG4FyBJYlmMPbEYnkdcr48F_baakbhQ"
+    "Authorization": ""
 }
 
 response = requests.get(url, headers=headers)
